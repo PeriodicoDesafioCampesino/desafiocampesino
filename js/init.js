21bab36ab3e3e8d9e58872a6f524e447fa70b089
PeriodicoDesafioCampesino <70114652+PeriodicoDesafioCampesino@users.noreply.github.com>
2020-08-23 19:42:40 -0500

    Add files via upload

--- js/init.js
+++ js/init.js
@@ -6,3 +6,13 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+		
+document.addEventListener('DOMContentLoaded', function() {
+    let sidenav = document.querySelectorAll('.sidenav');
+    let instancia_sidenav = M.Sidenav.init(sidenav,{});    
+    let dropdowns = document.querySelectorAll('.dropdown-trigger');
+    let instancia_dropwdown = M.Dropdown.init(dropdowns, {
+    hover:false});
+  });
